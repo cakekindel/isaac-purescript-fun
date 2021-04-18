54279069a45e0f08ac4976527adafa4fb93e3bbb
Orion Kindel <cakekindel@gmail.com>
2021-04-18 09:26:25 -0700

    add 01-syntax

---
+++ 01-syntax/src/Main.purs
@@ -0,0 +1,15 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+
+addInts :: Int -> Int -> Int
+
+-- mulInts
+-- addTwo
+
+main :: Effect Unit
+main = do
+  log "🍝"
---
+++ 01-syntax/test/Main.purs
@@ -0,0 +1,33 @@
+module Test.Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class.Console (log)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
+
+import Main
+
+main :: Effect Unit
+main = launchAff_ $ runSpec [consoleReporter] do
+  describe "addInts" do
+    it "`addInts 2 2` should equal `4`" do
+      (addInts 2 2) `shouldEqual` 4
+    it "`addInts 0 0` should equal `0`" do
+      (addInts 0 0) `shouldEqual` 0
+
+  describe "addTwo" do
+    it "`addTwo 2` should equal `4`" do
+      (addTwo 2) `shouldEqual` 4
+
+  describe "mulInts" do
+    it "`mulInts 0 0` should equal `0`" do
+      (mulInts 0 0) `shouldEqual` 0
+    it "`mulInts 10 0` should equal `0`" do
+      (mulInts 10 0) `shouldEqual` 0
+    it "`mulInts 10 2` should equal `20`" do
+      (mulInts 10 2) `shouldEqual` 20
