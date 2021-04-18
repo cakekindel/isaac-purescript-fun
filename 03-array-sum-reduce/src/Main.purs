204e4799cde1fa439569f592889a598ea2411894
Orion Kindel <cakekindel@gmail.com>
2021-04-18 11:29:46 -0700

    implement reduce

--- 03-array-sum-reduce/src/Main.purs
+++ 03-array-sum-reduce/src/Main.purs
@@ -2,10 +2,12 @@ module Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(Nothing, Just))
+import Data.Array ((!!))
 import Effect (Effect)
 import Effect.Console (log)
 
-reduce :: forall a b.
+reduce :: forall curr acc.
   (
     curr -> acc 
     -> acc
@@ -13,11 +15,12 @@ reduce :: forall a b.
     -> acc
     -> Array curr
     -> acc
+reduce reducer init arr = go 0 init
+  where go ix acc = case arr !! ix of
+                      Nothing -> acc
+                      Just a -> go (ix + 1) (reducer a acc)
 
-
-sum :: Array Int -> Int
-
-
+-- sum :: Array Int -> Int
 
 main :: Effect Unit
 main = do
--- 03-array-sum-reduce/test/Main.purs
+++ 03-array-sum-reduce/test/Main.purs
@@ -2,6 +2,7 @@ module Test.Main where
 
 import Prelude
 
+import Data.Array (snoc)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class.Console (log)
@@ -14,6 +15,7 @@ import Main
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
+  {-
   describe "sum" do
     it "sum empty array should equal 0" do
       (sum []) `shouldEqual` 0
@@ -22,12 +24,11 @@ main = launchAff_ $ runSpec [consoleReporter] do
     it "sum should sum" do
       (sum [1, 1]) `shouldEqual` 2
       (sum [1, 1, 1]) `shouldEqual` 3
+      -}
 
-  {-
   describe "reduce" do
-    let copy = \acc cur -> snoc acc cur
+    let copy = \cur acc -> snoc acc cur
     it "reduce should return accumulator on empty array" do
       (reduce copy [] []) `shouldEqual` []
     it "reduce should use reducer" do
       (reduce copy [] [1, 2, 3]) `shouldEqual` [1, 2, 3]
-  -}
